39fa2cf05d4c9447a7a672d1817e1a918da5c48b
flouthoc <flouthoc.git@gmail.com>
2025-02-12 11:44:14 -0800

    test: use /tmp as TMPDIR
    Signed-off-by: flouthoc <flouthoc.git@gmail.com>

--- tests/test_runner.sh
+++ tests/test_runner.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 # Default to using /var/tmp for test space, since it's more likely to support
 # labels than /tmp, which is often on tmpfs.
-export TMPDIR=${TMPDIR:-/var/tmp}
+export TMPDIR=${TMPDIR:-/tmp}
 
 function execute() {
 	>&2 echo "++ $@"
